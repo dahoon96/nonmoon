94c554442bf7de8d3b66a212ead512632674f4c9
moondahoon <kng03153@naver.com>
2020-08-13 16:37:55 +0900

    202008131637

---
+++ app/src/main/java/com/example/nonmoon/FirebaseID.java
@@ -0,0 +1,28 @@
+package com.example.nonmoon;
+
+public class FirebaseID {
+
+    public  static String user = "user";
+    public static String post = "post";
+    public static String Map = "Map";
+    public static String fitness = "fitness";
+    public static String latitude = "latitude";
+    public static String longitude = "longitude";
+    public static String name = "name";
+    public static String documentId = "documentId";
+    public static String birth = "birth";
+    public static String mobileNo = "mobileNo";
+    public static String email = "email";
+    public static String password = "password";
+    public static String nickname ="nickname";
+    public static String title = "title";
+    public static String contents = "contents";
+    public static String uri = "uri";
+    public static String timestamp = "timestamp";
+    public static String postId ="postId";
+    public static String replyName = "replyName";
+    public static String reply = "reply";
+    public static String replyId = "replyId";
+    public static String replyPostId = "replyPostId";
+    public static String pastPostId = "pastPostId";
+}
--- app/src/main/java/com/example/nonmoon/addLog.java
+++ app/src/main/java/com/example/nonmoon/addLog.java
@@ -1,5 +1,6 @@
 package com.example.nonmoon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,12 +8,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
-public class addLog extends AppCompatActivity {
-    private String confirm="";
+import java.util.HashMap;
+import java.util.Map;
+
+public class addLog extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+    private int confirm=0;
+    private EditText name, phonenumber, id, pw, pw2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +34,53 @@ public class addLog extends AppCompatActivity {
         setContentView(R.layout.activity_add_log);
 
         Button addlog = (Button)findViewById(R.id.addLog);
+        name = (EditText)findViewById(R.id.Signup_Name);
+        phonenumber = (EditText)findViewById(R.id.Signup_PhoneNumber);
+        id = (EditText)findViewById(R.id.Signup_Id);
+        pw = (EditText)findViewById(R.id.Signup_Pw);
+        pw2 = (EditText)findViewById(R.id.Signup_Pw2);
 
-        addlog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        addlog.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        confirm = 0;
+        if(id.getText().toString().equals("")||pw.getText().toString().equals("")||pw2.getText().toString().equals("")||phonenumber.getText().toString().equals("")||name.getText().toString().equals(""))
+            confirm = 1;
+        if(!pw.getText().toString().equals(pw2.getText().toString()))
+            confirm = 2;
+        if(confirm==0) {
+            mAuth.createUserWithEmailAndPassword(id.getText().toString(), pw.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Map<String, Object> userMap = new HashMap<>();
+                                if (user != null) {
+                                    userMap.put(FirebaseID.documentId, user.getUid());
+                                    userMap.put(FirebaseID.nickname, name.getText().toString());
+                                    userMap.put(FirebaseID.email, id.getText().toString());
+                                    userMap.put(FirebaseID.password, pw.getText().toString());
+                                    userMap.put(FirebaseID.mobileNo, phonenumber.getText().toString());
+                                    mStore.collection(FirebaseID.user).document(user.getUid()).set(userMap, SetOptions.merge());
+                                    finish();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), "동일 ID가 존재합니다.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+        else if(confirm == 1){
+            Toast.makeText(getApplicationContext(), "입력 하지 않은 부분이 존재 합니다.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else if(confirm == 2){
+            Toast.makeText(getApplicationContext(), "패스워드 두개가 다릅니다.",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
